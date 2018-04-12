9600a22f72d5e3b4fbe5eb2a378da2ae5d816338
mrudul03 <mrudul.palvankar@gmail.com>
2018-04-12 22:52:57 +0200

    cleaned

--- payment-account-service/src/main/java/com/microservices/account/gateway/CustomerEventConsumer.java
+++ payment-account-service/src/main/java/com/microservices/account/gateway/CustomerEventConsumer.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import com.microservices.account.contract.PaymentAccountResource;
